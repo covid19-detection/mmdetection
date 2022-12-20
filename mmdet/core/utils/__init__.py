b4e4835b2b4ec01cd6642d435f3427e552e68fb2
Linh T. Duong <linhduongtuan@yahoo.com>
2022-12-20 18:16:00 +0700

    Add files via upload

--- mmdet/core/utils/__init__.py
+++ mmdet/core/utils/__init__.py
@@ -1,13 +1,4 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .dist_utils import (DistOptimizerHook, all_reduce_dict, allreduce_grads,
-                         reduce_mean, sync_random_seed)
-from .misc import (center_of_mass, filter_scores_and_topk, flip_tensor,
-                   generate_coordinate, mask2ndarray, multi_apply,
-                   select_single_mlvl, unmap)
+from .collect_env import collect_env
+from .logger import get_root_logger
 
-__all__ = [
-    'allreduce_grads', 'DistOptimizerHook', 'reduce_mean', 'multi_apply',
-    'unmap', 'mask2ndarray', 'flip_tensor', 'all_reduce_dict',
-    'center_of_mass', 'generate_coordinate', 'select_single_mlvl',
-    'filter_scores_and_topk', 'sync_random_seed'
-]
+__all__ = ['get_root_logger', 'collect_env']
